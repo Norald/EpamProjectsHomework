7dd955a2552619b5e8372a4465250fb8b46485f7
WUTZMpon <dp260296pvm@gmail.com>
2020-11-24 13:01:31 +0200

    Fixed lombock annotations
    Fixed lombock annotations, deleted unused imports, changed packaging to jar

--- homework_5/demo/src/main/java/com/epam/demo/controller/MainController.java
+++ homework_5/demo/src/main/java/com/epam/demo/controller/MainController.java
@@ -3,7 +3,6 @@ package com.epam.demo.controller;
 import com.epam.demo.model.User;
 import com.epam.demo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- homework_5/demo/src/main/java/com/epam/demo/model/User.java
+++ homework_5/demo/src/main/java/com/epam/demo/model/User.java
@@ -12,37 +12,27 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
+@Getter
+@Setter
 @ToString(exclude = "password")
 public class User {
 
     @Id
-    @Getter
-    @Setter
     private int id;
 
     @Column(name = "email")
-    @Getter
-    @Setter
     private String email;
 
     @Column(name = "idn")
-    @Getter
-    @Setter
     private long idn;
 
     @Column(name = "block")
-    @Getter
-    @Setter
     private boolean blocked;
 
     @Column(name = "user_role_id")
-    @Getter
-    @Setter
     private int userRoleId;
 
     @Column(name = "password")
-    @Getter
-    @Setter
     private String password;
 
 }
